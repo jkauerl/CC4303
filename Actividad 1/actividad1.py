398493a44b761792deb8b7607065f8cbedfd958c
jkauerl <kauer.javier01@gmail.com>
2023-08-24 12:33:42 -0400

    almost finished activity 1

--- Actividad 1/actividad1.py
+++ Actividad 1/actividad1.py
@@ -5,11 +5,27 @@ from utils import *
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 # definimos dirección donde queremos que correr el server_socket
-server_address = ('localhost', 9000)
+server_address = ('localhost', 8000)
 
 # hacemos bind del server socket a la dirección server_address
 server_socket.bind(server_address)
+
+# parametros para cambiar
 buff_size = 1024 # En bytes
+# definimos el path donde se encuentra el archivo que queremos enviar
+path = f"Actividad 1/actividad1.html"
+file = open(path, "r")
+response_body = file.read()
+response_head = """HTTP/1.1 200 OK
+Server: nginx/1.17.0
+Date: Thu, 24 Aug 2023 15:54:05 GMT
+Content-Type: text/html; charset=utf-8
+Content-Length: 145
+Connection: keep-alive
+Access-Control-Allow-Origin: *
+X-ElQuePregunta: Javier Kauer
+
+"""
 
 # luego con listen (función de sockets de python) le decimos que puede
 # tener hasta 3 peticiones de conexión encoladas
@@ -33,6 +49,13 @@ while True:
     print("a")
     http = create_HTTP_message(mensaje)
     print(http)
+
+    print("test")
+    print(response_head + response_body)
+
+    http_response = response_head + response_body
+
+    new_socket.send(http_response.encode())
 
     # cerramos la conexión
     # notar que la dirección que se imprime indica un número de puerto distinto al 8000
--- Actividad 1/test.py
+++ Actividad 1/test.py
@@ -1,6 +1,11 @@
-test = b"1 2 3 \r\n\r\n a"
+""" test = b"1 2 3 \r\n\r\n a"
 
 print(test)
 print(test.decode())
 print(test.decode().split("\r\n"))
+ """
 
+
+import os
+
+print(os.getcwd())
